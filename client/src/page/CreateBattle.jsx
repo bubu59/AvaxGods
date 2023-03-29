72355b4205dbcde11682d774710d60b210d61189
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-03-29 17:54:09 +0800

    chore: gameLoad screen

--- client/src/page/CreateBattle.jsx
+++ client/src/page/CreateBattle.jsx
@@ -2,16 +2,29 @@ import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import styles from '../styles'
 import { useGlobalContext } from '../context'
-import {PageHOC, CustomButton, CustomInput} from '../components'
+import { PageHOC, CustomButton, CustomInput, GameLoad } from '../components'
 
 const CreateBattle = () => {
   const navigate = useNavigate()
   const {contract, battleName, setBattleName} = useGlobalContext()
-  const handleClick = () => {
+  const [waitBattle, setWaitBattle] = useState(false)
 
+  const handleClick = async () => {
+    if(battleName || battleName.trim()) {
+      return null
+    }
+
+    try{
+      await contract.createBattle(battleName)
+      setWaitBattle(true)
+    }catch(error){
+      console.log(error)
+    }
   }
   return (
     <>
+      {waitBattle && <GameLoad/>}
+
       <div className='flex flex-col mb-5'>
          <CustomInput
           label='Battle'
