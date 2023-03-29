d85246784cbb7a03f8e249b28a1098c051f9002d
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-03-29 17:20:09 +0800

    chore: join battle page

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useGlobalContext } from '../context'
+import { PageHOC, CustomButton } from '../components'
+import styles from '../styles'
 
 const JoinBattle = () => {
+    const navigte = useNavigate()
+
   return (
-    <div>JoinBattle</div>
+    <>
+        <h2 className={styles.joinHeadText}>Available Battles:</h2>
+        <p className={styles.infoText} onClick={() => navigte('/create-battle')}>Or create a new battle!</p>
+    </>
   )
 }
 
-export default JoinBattle
+export default PageHOC(
+    JoinBattle,
+    <>Join <br/> a Battle</>,
+    <>Join existing battles!</>
+)
