0f93340cec2170eb6be11ff00f3e42bf2372cf95
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-03-30 15:02:43 +0800

    chore: Layout of battle + playerInfo component

--- client/src/components/PlayerInfo.jsx
+++ client/src/components/PlayerInfo.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
+import styles from '../styles'
 
-const PlayerInfo = () => {
+const healthPoints = 25
+
+const PlayerInfo = ({player, playerIcon, mt}) => {
   return (
-    <div>PlayerInfo</div>
+    <div className={`${styles.flexCenter} ${mt ? 'mt-4' : 'mb-4'}`}>
+        <img 
+            data-for={`Player-${mt ? '1' : '2'}`} 
+            data-tip 
+            src={playerIcon} 
+            alt='player-2' 
+            className='w-14 h-14 object-contain rounded-full'
+        />
+
+        <div 
+            data-for={`Health--${mt ? '1' : '2'}`}
+            data-tip={`Health: ${player?.health}`}
+            className={styles.playerHealth}
+        >
+            {[]}
+        </div>
+    </div>
   )
 }
 
--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import styles from '../styles'
-import { Alert, PlayerInfo } from '../components'
+import { Alert, PlayerInfo, GameInfo, Card, ActionButton } from '../components'
 import { useGlobalContext } from '../context'
 import {attack, attackSound, defense, defenseSound, player01 as player01Icon, player02 as player02Icon} from "../assets"
 import {playAudio} from '../utils/animation.js'
@@ -56,11 +56,48 @@ const Battle = () => {
   return (
     <div className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}>
          {showAlert?.status && <Alert type={showAlert.type} message={showAlert.message}/>}
+        
         <PlayerInfo 
             player={player02}
             playerIcon={player02Icon}
             mt
         />
+
+        <div className={`${styles.flexCenter} flex-col my-10`}>
+            <Card
+                card={player02}
+                title={player02?.playerName}
+                cardRef=''
+                playerTwo
+            />
+
+            <div className='flex items-center flex-row'>
+                <ActionButton   
+                    imgUrl={attack}
+                    handleClick={() => {}}
+                    restStyles='mr-2 hover:border-yellow-400'
+                />
+                <Card
+                    card={player01}
+                    title={player01?.playerName}
+                    cardRef=''
+                    restStyles='mt-3'
+                />
+                <ActionButton   
+                    imgUrl={defense}
+                    handleClick={() => {}}
+                    restStyles='ml-6 hover:border-red-600'
+                />
+            </div>
+        </div>
+
+        <PlayerInfo 
+            player={player01}
+            playerIcon={player01Icon}
+            mt
+        />
+
+        <GameInfo/>
     </div>
   )
 }
